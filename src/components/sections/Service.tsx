9c4d72e30eb99f47825f3858fcb291371d70e3b9
Roju Ghimire <ghimireroju@gmail.com>
2024-08-23 19:36:36 +0545

    changes

--- src/components/sections/Service.tsx
+++ src/components/sections/Service.tsx
@@ -9,17 +9,18 @@ interface ServiceCardProps {
 
 const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, imageUrl, label }) => {
   return (
-    <div className="flex flex-col w-full p-4 lg:w-[500px]">
+    <div className="flex flex-col w-full  lg:w-[500px]">
       <div className="relative">
-        <img src={imageUrl} alt={title} className="h-64 w-full lg:w-[500px] rounded-xl lg:h-72 object-cover" />
+        <img src={imageUrl} alt={title} className="h-64 w-full lg:w-[500px] rounded-xl lg:h-[330px] object-cover" />
         <div className="absolute top-0 left-1/2 transform -translate-x-1/2 bg-secondary text-white text-center py-3 px-6 rounded-lg font-overpass transition-all hover:bg-primary">
           {label}
         </div>
-      </div>
-      <div className="bg-slate-100 text-center p-4 rounded-b-lg shadow-lg mt-4">
-        <h3 className="text-xl font-semibold mb-2">{title}</h3>
+        <div className="bg-[#F6ECED] w-[309px] h-[168.19px] absolute top-[227px]  ml-[90px] pt-[15.6px] gap-[15.6px] rounded-lg px-4 font-overpass ">
+        <h3 className="text-xl ml-12 font-semibold mb-2">{title}</h3>
         <p className="text-gray-600 text-sm">{description}</p>
       </div>
+      </div>
+      
     </div>
   );
 };
@@ -50,13 +51,13 @@ const Services: React.FC = () => {
   ];
 
   return (
-    <section className="py-12">
-      <div className=" mx-auto px-4 sm:px-8 lg:px-16">
+    <section className="py-12 lg:h-[605px]">
+      <div className="mx-auto  px-4 sm:px-8 lg:px-16">
         <h2 className="text-3xl font-overpass font-bold text-center mb-4">Services</h2>
         <p className="text-center text-gray-500 font-overpass mb-12">
           At Skill Spot Australia, we provide top-notch facilities for effective learning
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 gap-8 lg:gap-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-[50px] lg:px-8 ">
           {services.map((service, index) => (
             <ServiceCard
               key={index}
