79e48dae9c871bd2487e6f449308327b67e0cc22
Roju Ghimire <ghimireroju@gmail.com>
2024-08-30 22:14:19 +0545

    made hero section and frame responsive

--- src/components/sections/Frame.tsx
+++ src/components/sections/Frame.tsx
@@ -8,48 +8,58 @@ const frameVariants = {
 
 const Frame: React.FC = () => {
   return (
-    <div className="w-full  flex flex-col md:flex-row font-overpass">
+    <div className="w-full flex flex-col md:flex-row  font-overpass">
       <motion.div
-        className="flex-1 h-[120px]  md:h-[99px] bg-[#001B6A] p-4 md:p-[25px_89px_25px_20px] text-white"
+        className="flex-1 h-[120px]  md:h-[140px] lg:h-[100px] p-4 md:p-6 lg:p-[25px_20px_25px_89px] bg-[#001B6A] text-white"
         variants={frameVariants}
         initial="hidden"
         animate="visible"
         transition={{ duration: 1 }}
       >
-        <h3 className="text-base md:text-lg font-bold">ODL mode of study</h3>
-        <p className="text-xs md:text-sm">Study in your own place and time</p>
-      </motion.div>
-      <motion.div
-        className="flex-1 h-[120px] md:h-[99px] p-4 md:p-[25px_89px_25px_20px] bg-[#CB2026] text-white"
-        variants={frameVariants}
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 1 }}
-      >
-        <h3 className="text-base md:text-lg font-bold">Flexibility</h3>
-        <p className="text-xs md:text-sm">Self-Individualized Learning</p>
-      </motion.div>
-      <motion.div
-        className="flex-1 h-[120px] md:h-[99px] p-4 md:p-[25px_89px_25px_20px] bg-[#001B6A] text-white"
-        variants={frameVariants}
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 1 }}
-      >
-        <h3 className="text-base md:text-lg font-bold">
-          Certificate valid all over the world
+        <h3 className="text-sm md:text-lg lg:text-xl font-bold">
+          ODL mode of study
         </h3>
-        <p className="text-xs md:text-sm">International Skill</p>
+        <p className="text-xs md:text-base lg:text-lg">
+          Study in your own place and time
+        </p>
       </motion.div>
       <motion.div
-        className="flex-1 h-[120px] md:h-[99px] p-4 md:p-[25px_89px_25px_20px] bg-[#CB2026] text-white"
+        className="flex-1 h-[120px] md:h-[140px] lg:h-[100px] p-4 md:p-6 lg:p-[25px_20px_25px_89px] bg-[#CB2026] text-white"
         variants={frameVariants}
         initial="hidden"
         animate="visible"
-        transition={{ duration: 1 }}
+        transition={{ duration: 2 }}
       >
-        <h3 className="text-base md:text-lg font-bold">Blended Learning</h3>
-        <p className="text-xs md:text-sm">Effective Student support system</p>
+        <h3 className="text-sm md:text-lg lg:text-xl font-bold">Flexibility</h3>
+        <p className="text-xs md:text-base lg:text-lg">
+          Self-Individualized Learning
+        </p>
+      </motion.div>
+      <motion.div
+        className="flex-1 h-[120px] md:h-[140px] lg:h-[100px] p-4 md:p-6 lg:p-[25px_20px_25px_89px] bg-[#001B6A] text-white"
+        variants={frameVariants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 3 }}
+      >
+        <h3 className="text-sm md:text-lg lg:text-xl font-bold">
+          All over the world Certified
+        </h3>
+        <p className="text-xs md:text-base lg:text-lg">International Skill</p>
+      </motion.div>
+      <motion.div
+        className="flex-1 h-[120px] md:h-[140px] lg:h-[100px] p-4 md:p-6 lg:p-[25px_20px_25px_89px] bg-[#CB2026] text-white"
+        variants={frameVariants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 4 }}
+      >
+        <h3 className="text-sm md:text-lg lg:text-xl font-bold">
+          Blended Learning
+        </h3>
+        <p className="text-xs md:text-base lg:text-lg">
+          Effective Student support system
+        </p>
       </motion.div>
     </div>
   );
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -13,7 +13,8 @@ const Hero: React.FC = () => {
   });
 
   return (
-    <div className="relative h-auto lg:h-[577px] px-10 lg:px-40 mt-12 lg:mt-24 w-full text-white  font-overpass">
+    <div className="relative h-[800px] lg:h-[577px] px-5 lg:px-20 mt-10 md:mt-20 lg:mt-24 w-full text-white font-overpass flex items-center justify-center">
+      {/* Background Image and Gradient */}
       <div className="absolute inset-0 z-0">
         <motion.img
           src="/BG.jpeg"
@@ -21,63 +22,59 @@ const Hero: React.FC = () => {
           className="object-cover w-full h-full"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 1.5 }}
+          transition={{ duration: 3.5 }}
         />
         <div className="absolute inset-0 bg-gradient-to-r from-[#203180] via-[#18255c66] to-[#12121366] opacity-85"></div>
       </div>
-      <div className="absolute flex flex-row h-[334px]  lg:w-[1440px] gap-[95px] top-[146px] left-[200px] ">
+
+      {/* Content */}
+      <div className="relative z-10 flex flex-col lg:flex-row h-full w-full lg:max-w-[1440px] gap-8 lg:gap-[95px] items-center justify-center">
         {/* Left Content */}
         <motion.div
-          className="flex flex-col  lg:items-start justify-center gap-6 lg:gap-[42px] h-auto lg:h-[297px] w-full lg:w-[70%]"
+          className="flex flex-col items-center lg:items-start justify-center gap-6 lg:gap-[42px] text-center lg:text-left w-full lg:w-[60%] px-4 lg:px-0"
           initial={{ x: -100, opacity: 0 }}
           animate={{ x: textInView ? 0 : -100, opacity: textInView ? 1 : 0 }}
-          transition={{ duration: 1.5 }}
+          transition={{ duration: 3.5 }}
           ref={textRef}
         >
-          <h1 className="text-xl font-overpass sm:text-3xl lg:text-4xl leading-snug lg:leading-[53.17px] font-extrabold text-center lg:text-left w-full ">
+          <h1 className="text-2xl sm:text-3xl items-center justify-center lg:text-4xl leading-snug lg:leading-[53.17px] font-extrabold">
             Welcome to Skill Spot
             <br />
-            <span className="block text-center lg:text-center lg:justify-center lg:w-[60%]">
-              Australia
-            </span>
+            <span>Australia</span>
           </h1>
 
-          <p className="text-sm sm:text-base lg:text-lg font-bold leading-normal lg:leading-[30px] text-center lg:text-left w-full">
-            Welcome to Skill Spot Australia Pty Ltd, your RPL destination
-            nationwide.
+          <p className="text-sm sm:text-base lg:text-lg font-bold leading-normal lg:leading-[30px]">
+            Welcome to Skill Spot Australia Pty Ltd, your RPL destination nationwide.
             <br />
             Celebrating your unique journey, we recognize and honor your skills.
             <br />
-            Unlocking potential, we help you achieve formal qualifications
-            effortlessly.
+            Unlocking potential, we help you achieve formal qualifications effortlessly.
           </p>
-          <div className="flex flex-row md:px-44 lg:px-0 gap-4 items-center justify-center lg:justify-start">
-            <motion.button
-              className="bg-secondary hover:bg-primary font-bold text-white px-4 py-2 rounded-lg h-[45px] w-[300px]"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              transition={{ duration: 0.3 }}
-            >
-              Free 60 second Skill Assesment
-            </motion.button>
-          </div>
+          <motion.button
+            className="bg-secondary hover:bg-primary font-bold text-white px-4 py-2 rounded-lg h-[45px] w-full sm:w-[300px]"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ duration: 3.3 }}
+          >
+            Free 60 second Skill Assessment
+          </motion.button>
         </motion.div>
 
         {/* Right Content - Image */}
         <motion.div
-          className="relative lg:px-20 mt-8 lg:mt-0 flex justify-center w-full lg:w-[70%] "
+          className="relative flex justify-center items-center w-full lg:w-[40%] mt-8 lg:mt-0"
           initial={{ scale: 0.9, opacity: 0 }}
           animate={{
             scale: imageInView ? 1 : 0.9,
             opacity: imageInView ? 1 : 0,
           }}
-          transition={{ duration: 1.5 }}
+          transition={{ duration: 2.5 }}
           ref={imageRef}
         >
           <motion.img
             src="/small.jpeg"
             alt="Small Image"
-            className="hidden lg:flex shadow-lg rounded-tl-[146.72px]  rounded-br-[146.72px] w-[300px] sm:w-[400px] lg:w-[550px] h-auto lg:h-[334px]"
+            className="shadow-lg rounded-tl-[146.72px] rounded-br-[146.72px] w-full sm:w-[350px] lg:w-[550px] h-full sm:h-[350px] lg:h-[350px] object-cover"
             style={{
               borderTop: "none",
               borderRight: "5.14px solid #FFFFFF",
@@ -89,7 +86,7 @@ const Hero: React.FC = () => {
             transition={{ duration: 1.5 }}
           />
 
-          <button className="hidden lg:block text-md absolute transform translate-x-[300%] -translate-y-1/2 w-[85.45px] h-[86.45px] bg-[linear-gradient(270deg,_#C2114C_31.5%,_#001B6A_78.5%)] rounded-full flex-col justify-center items-center text-white font-extrabold">
+          <button className="absolute -top-2 -right-2 sm:-top-8 sm:right-36 lg:-top-10 lg:-right-10 text-md w-[60px] h-[60px] sm:w-[70px] sm:h-[70px] lg:w-[85px] lg:h-[85px] bg-[linear-gradient(270deg,_#C2114C_31.5%,_#001B6A_78.5%)] rounded-full flex-col justify-center items-center text-white font-extrabold">
             Apply <br /> Now!
           </button>
         </motion.div>
